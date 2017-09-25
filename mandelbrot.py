c839faf712c16eba84bc884e1e58fb7c80da764a
Jared Skinner <jaredds66@gmail.com>
2017-09-25 11:36:03 -0500

    simplified logic a hair

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
 import numpy as np
+import time
+import matplotlib
+from matplotlib import colors
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import argparse
 
 def mandelbrot_set(xmin, xmax, ymin, ymax, xn, yn, maxiter, horizon=2.0):
     X = np.linspace(xmin, xmax, xn, dtype=np.float32)
@@ -19,47 +25,21 @@ def mandelbrot_set(xmin, xmax, ymin, ymax, xn, yn, maxiter, horizon=2.0):
     return Z, N
 
 
-if __name__ == '__main__':
-    import time
-    import matplotlib
-    from matplotlib import colors
-    matplotlib.use('Agg')
-    import matplotlib.pyplot as plt
-    import argparse
-
+def main():
     parser = argparse.ArgumentParser(description='mandelbrot', prog="mandlebrot.py")
     parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.0.1.0')
-    parser.add_argument('--xmin', help="The minimum x value")
-    parser.add_argument('--xmax', help="The maxiumu x value")
-    parser.add_argument('--ymin', help="The minimum y value")
-    parser.add_argument('--ymax', help="The maximum y value")
-    parser.add_argument('--iter', help="The number of iterations")
+    parser.add_argument('--xmin', help="The minimum x value", default=-2)
+    parser.add_argument('--xmax', help="The maxiumu x value", default=2)
+    parser.add_argument('--ymin', help="The minimum y value", default=-2)
+    parser.add_argument('--ymax', help="The maximum y value", default=2)
+    parser.add_argument('--iter', help="The number of iterations", default=10)
     args = parser.parse_args()
 
-    if args.xmin:
-        xmin = float(args.xmin)
-    else:
-        xmin = -2
-
-    if args.xmax:
-        xmax = float(args.xmax)
-    else:
-        xmax = 2
-
-    if args.ymin:
-        ymin = float(args.ymin)
-    else:
-        ymin = -2
-
-    if args.ymax:
-        ymax = float(args.ymax)
-    else:
-        ymax = 2
-
-    if args.xmin:
-        maxiter = int(args.iter)
-    else:
-        maxiter = 10 
+    xmin = float(args.xmin)
+    xmax = float(args.xmax)
+    ymin = float(args.ymin)
+    ymax = float(args.ymax)
+    maxiter = int(args.iter)
 
     xn = 3000/2
     yn = 2500/2
@@ -86,3 +66,7 @@ if __name__ == '__main__':
         plt.savefig(str(i)+".png")
 
         plt.close(fig)
+
+
+if __name__ == '__main__':
+    main()
